98da054f511a796cb216efd2e43e5c0ebd43d291
Abramov Denis <qbra.move@gmail.com>
2023-08-03 13:28:04 +0300

    Scale and axes improved.

--- vis_lidar_matplotlib.py
+++ vis_lidar_matplotlib.py
@@ -13,7 +13,7 @@ ax.set_xlim(500, 600)  # assuming the x range is known
 ax.set_ylim(-1, 1)  # adjust y range according to your data
 ax.set_thetamin(0)
 ax.set_thetamax(180)
-ax.set_rlim(1000)
+ax.set_rmax(1000)
 
 
 # generator function to simulate incoming stream data
@@ -31,7 +31,7 @@ def init():
 
 
 def update(data):
-    angles = np.arange(0.0, 180.0, 721 / 180)
+    angles = np.arange(0.0, 180.0, 180 / 721)
     dists = data
     for i in range(min(len(dists), len(angles))):
         line.set_data(np.append(line.get_xdata(), dists[i]),
