8d376689e9960822c3c181b7d07c2e08a545ddda
Abramov Denis <qbra.move@gmail.com>
2023-08-03 19:58:40 +0300

    New drawing function.

--- vis_lidar_matplotlib.py
+++ vis_lidar_matplotlib.py
@@ -1,49 +1,52 @@
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from math import sin, cos
+from matplotlib.animation import FuncAnimation
 
+# set up the plot
 fig = plt.figure()
 ax = fig.add_subplot(111, polar=True)
 
-# assuming the streaming data is 1D
-line, = ax.plot([], [], lw=2)
-
-ax.set_xlim(500, 600)  # assuming the x range is known
-ax.set_ylim(-1, 1)  # adjust y range according to your data
-ax.set_thetamin(0)
-ax.set_thetamax(180)
-ax.set_rmax(1000)
+# initialize an empty line plot for the polygon
+line, = ax.plot([], [], 'r-', alpha=0.5)  # Red line connecting the points
 
 
-# generator function to simulate incoming stream data
-def data_gen():
-    with open('lidar.raw', 'rb') as f:
-        while buf := f.read(2 + 8 + 1 + 721 * 2 + 1 + 4 + 2):
+# function to initialize the plot
+def init():
+    line.set_data([], [])  # Initialize the line plot with empty data
+    return line,
+
+
+# function to update the plot
+def update(data):
+    dists, angles = zip(*data)  # Unzip the data
+    dists = np.asarray(dists)
+    angles = np.asarray(angles)
+
+    x = angles
+    y = dists
+
+    # update the line plot with the polygon
+    line.set_data(np.append(x, x[0]), np.append(y, y[0]))  # Close the polygon
+
+    ax.set_rlim(0, dists.mean() / 2)
+
+    return line,
+
+
+# function to read data from 'lidar.raw' in a loop
+def read_lidar_data():
+    angles = np.linspace(0.0, np.pi, 721)
+    while True:
+        with open('lidar3.raw', 'rb') as f:
+            buf = f.read(2 + 8 + 1 + 721 * 2 + 1 + 4 + 2)
             dists_time = struct.unpack_from('>721H', buf, 11)
             dists = np.asarray(dists_time)
-            yield dists
+            yield list(zip(dists, angles))
 
 
-def init():
-    line.set_data([], [])
-    return line,
+# create the animation
+ani = FuncAnimation(fig, update, frames=read_lidar_data, init_func=init, blit=True, interval=100)
 
-
-def update(data):
-    angles = np.arange(0.0, 180.0, 180 / 721)
-    angle = []
-    for i in range(720):
-        angle[i] = np.deg2rad(angles[i])
-    dists = data
-    for i in range(min(len(dists), len(angles))):
-        line.set_data(np.append(line.get_xdata(), sin(angle[i])*dists[i]),
-                      np.append(line.get_ydata(), cos(angle[i])*dists[i]))
-
-    return line,
-
-
-ani = animation.FuncAnimation(fig, update, frames=data_gen, init_func=init, blit=True)
-
+# show the plot
 plt.show()
